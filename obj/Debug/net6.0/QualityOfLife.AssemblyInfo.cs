170f16612d059e7daed93e5fc3a128171de58a43
Loner1536 <Woojuiscute@gmail.com>
2025-04-12 18:09:57 -0500

    Added 360 FPS Limit and Smooth Button In and Out Fades

--- obj/Debug/net6.0/QualityOfLife.AssemblyInfo.cs
+++ obj/Debug/net6.0/QualityOfLife.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QualityOfLife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6bf957297b467fa0bcfcfa8ea4fa44d395ab3aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788bbf31ff6a974fc5cbc6b26bf64a7f91e20a56")]
 [assembly: System.Reflection.AssemblyProductAttribute("QualityOfLife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QualityOfLife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
