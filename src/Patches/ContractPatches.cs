9d68e49fe3f2a5af41587a41330eb4270ce2df6b
Loner1536 <Woojuiscute@gmail.com>
2025-04-16 01:47:32 -0500

    Fix Contract Patch

--- src/Patches/ContractPatches.cs
+++ src/Patches/ContractPatches.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Reflection;
 using ScheduleOne.GameTime;
 using ScheduleOne.Quests;
 using HarmonyLib;
@@ -8,71 +6,66 @@ using UnityEngine.UI;
 
 namespace ContractCompassMarkersPatch
 {
-    [HarmonyPatch(typeof(Contract))]
-    public class ContractPatch
+    [HarmonyPatch(typeof(Contract), nameof(Contract.Start))]
+    public class ContractStartPatch
     {
-        public static IEnumerable<MethodBase> TargetMethods()
-        {
-            yield return AccessTools.Method(typeof(Contract), "Start");
-            yield return AccessTools.Method(typeof(Contract), "CheckExpiry");
-        }
-
         [HarmonyPostfix]
-        private static void Postfix(MethodBase __originalMethod, Contract __instance)
+        private static void Postfix(Contract __instance)
         {
-            if (__originalMethod.Name == "Start")
+            foreach (var questEntry in __instance.Entries)
             {
-                foreach (var questEntry in __instance.Entries)
+                if (questEntry.compassElement != null && questEntry.isActiveAndEnabled)
                 {
-                    if (questEntry.compassElement != null && questEntry.isActiveAndEnabled)
-                    {
-                        questEntry.compassElement.Visible = false;
-                    }
+                    questEntry.compassElement.Visible = false;
                 }
-                return;
             }
+        }
+    }
 
-            if (__originalMethod.Name == "CheckExpiry")
+    [HarmonyPatch(typeof(Contract), nameof(Contract.CheckExpiry))]
+    public class ContractCheckExpiryPatch
+    {
+        [HarmonyPostfix]
+        private static void Postfix(Contract __instance)
+        {
+            foreach (var questEntry in __instance.Entries)
             {
-                foreach (var questEntry in __instance.Entries)
+                if (questEntry.compassElement != null)
                 {
-                    if (questEntry.compassElement != null)
+                    bool inWindow = TimeManager.Instance.IsCurrentTimeWithinRange(
+                        __instance.DeliveryWindow.WindowStartTime,
+                        __instance.DeliveryWindow.WindowEndTime
+                    );
+
+                    Transform icon = questEntry.compassElement.Rect.Find("ContractIcon(Clone)");
+                    if (icon == null) continue;
+
+                    Transform fill = icon.Find("Fill");
+                    Transform background = icon.Find("Background");
+
+                    Color backgroundColor = Color.grey;
+
+                    if (__instance.expiryReminderSent)
                     {
-                        bool inWindow = TimeManager.Instance.IsCurrentTimeWithinRange(
-                            __instance.DeliveryWindow.WindowStartTime,
-                            __instance.DeliveryWindow.WindowEndTime
-                        );
-
-                        Transform icon = questEntry.compassElement.Rect.Find("ContractIcon(Clone)");
-                        if (icon == null) continue;
-
-                        Transform fill = icon.Find("Fill");
-                        Transform background = icon.Find("Background");
-
-                        Color backgroundColor = Color.grey;
-
-                        if (__instance.expiryReminderSent)
-                        {
-                            backgroundColor = new Color(0.8f, 0.4f, 0.4f, 0.8f);
-                        }
-                        else if (inWindow)
-                        {
-                            backgroundColor = new Color(0.4f, 0.8f, 0.4f, 0.8f);
-                        }
-
-                        Image fillImage = fill.GetComponent<Image>();
-                        if (fillImage != null)
-                            fillImage.color = Color.white;
-
-                        Image bgImage = background.GetComponent<Image>();
-                        if (bgImage != null)
-                            bgImage.color = backgroundColor;
-
-                        questEntry.compassElement.Visible =
-                            questEntry.ShouldShowPoI() &&
-                            questEntry.isActiveAndEnabled &&
-                            __instance.QuestState == EQuestState.Active;
+                        backgroundColor = new Color(0.8f, 0.4f, 0.4f, 0.8f);
                     }
+                    else if (inWindow)
+                    {
+                        backgroundColor = new Color(0.4f, 0.8f, 0.4f, 0.8f);
+                    }
+
+                    Image fillImage = fill.GetComponent<Image>();
+                    if (fillImage != null)
+                        fillImage.color = Color.white;
+
+                    Image bgImage = background.GetComponent<Image>();
+                    if (bgImage != null)
+                        bgImage.color = backgroundColor;
+
+                    questEntry.compassElement.Visible =
+                        questEntry.ShouldShowPoI() &&
+                        questEntry.isActiveAndEnabled &&
+                        __instance.QuestState == EQuestState.Active;
                 }
             }
         }
